9d10ab0a6cc2d705778d14110ef59bfde56fc5c2
Albert Chu <albertchu539@gmail.com>
2022-04-17 21:39:16 -0600

    Overhauled experience items to be prettier

--- components/experience-item/experience-item.js
+++ components/experience-item/experience-item.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import styles from './experience-item.module.css'
 
-export const ExperienceItem = ({ company, role, date, children }) => {
+export const ExperienceItem = ({ company, companyUrl, role, date, children }) => {
     return (
         <div className={styles.container}>
-            <div className={styles.titleBlock}>
-                <div className={styles.company}>{company}</div>
-                <div className={styles.role}>{role}</div>
+            <div className={styles.row}>
+                <div className={styles.titleBlock}>
+                    <div className={styles.company}>{
+                        companyUrl ?
+                            <a href={companyUrl}>
+                                {company}
+                            </a> :
+                            company
+                    }</div>
+                    <div className={styles.role}>{role}</div>
+                </div>
+                <div className={styles.date}>{date}</div>
             </div>
-            <div className={styles.date}>{date}</div>
             {children}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
         <div className={styles.contents}>
           <LabelledSection title="About Me">
             <p>
-              I&apos;m a web developer by trade that enjoys pragmatically solving complex problems. I am comfortable working all over the tech stack but I hold a special spot in my heart for frontend development. Building great user experiences while bringing beautiful designs to life is a driving force in my career. I want to make major impacts in interesting projects that deliver value to users in meaningful ways.
+              Born and raised in Canada, I&apos;m a web developer by trade that enjoys pragmatically solving complex problems. I am comfortable working all over the tech stack but I hold a special spot in my heart for frontend development. Building great user experiences while bringing beautiful designs to life is a driving force in my career. I want to make major impacts in interesting projects that deliver value to users in meaningful ways.
             </p>
             <br />
             <p>
@@ -54,7 +54,7 @@ export default function Home() {
             </p>
             <br />
 
-            <p>If you are a prospective employer with an exciting opportunity, please include the phrase &quot;Pineapples belong on pizza&quot; somewhere in your email. This will show that you have taken the time to evaluate me for skill fit and I promise that I will evaluate your role with as thorough attention as you have given my profile.</p>
+            <p>If you are a prospective employer with an exciting opportunity, please include the phrase &quot;Pineapples belong on pizza&quot; somewhere in your email. This will show that you have taken the time to evaluate me for skill fit and I promise that I will evaluate your role as thoroughly as you have evaluated my profile.</p>
           </LabelledSection>
 
           <LabelledSection title="Experience">
@@ -62,6 +62,7 @@ export default function Home() {
               company="ZenHub"
               role="Senior Frontend Developer"
               date="August 2021 - Present"
+              companyUrl="https://www.zenhub.com/"
             >
               <p>
                 I focused on developing features that would improve the developer experience in project management. As a senior member of the team, I am a thought leader for development efforts on complex features. My responsibilities are a balance between development and communicating with PMs and QAs to address priorities quickly. I have a critical role in developing and launching the ZenHub Platform.
@@ -72,6 +73,7 @@ export default function Home() {
               company="Ambyint"
               role="Senior Software Developer"
               date="May 2019 - August 2021"
+              companyUrl="https://www.ambyint.com/"
             >
               <p>
                 Senior member of an agile team building a lift-optimization platform agnostic to lift types. Focused on building a highly functional platform to innovate and disrupt the Oil and Gas sector. Distributed Nodejs backend built in the CQRS pattern using Kafka backed onto MongoDB.
@@ -83,6 +85,7 @@ export default function Home() {
               company="Replicon"
               role="Software Developer"
               date="May 2017 - May 2019"
+              companyUrl="https://www.replicon.com/en-ca/"
             >
               <p>
                 Full-stack dev on a productivity cloud platform. Node.js backend deployed on AWS and using a mix of AWS technologies. React.js frontend using GraphQL as a middleware to query a C# backend. Worked closely with the UX team to iterate and develop prototype designs into deployable production ready code.
@@ -95,6 +98,7 @@ export default function Home() {
               company="Synopsys"
               role="Software Engineer"
               date="May 2015 - May 2017"
+              companyUrl="https://www.synopsys.com/software-integrity/security-testing/static-analysis-sast.html"
             >
               <p>
                 Spearheaded the performance lab to deliver data visualizations to identify bottlenecks and regressions in weekly builds. Additionally, I built out large features for automated testing and triage of the Coverity platform.
@@ -106,6 +110,7 @@ export default function Home() {
               company="Coverity"
               role="Software Automation Intern"
               date="Oct 2013 - May 2015"
+              companyUrl="https://www.synopsys.com/software-integrity/security-testing/static-analysis-sast.html"
             >
               <p>
                 My first steps into the industry of software development. My main work was around end to end test automation, and performance and scalability testing on unix systems to identify bottlenecks in the static analysis engine.
@@ -121,9 +126,10 @@ export default function Home() {
               company="University of Calgary"
               role="Bachelors of Science"
               date="Sept 2010 - May 2015"
+              companyUrl="https://science.ucalgary.ca/"
             >
               <p>
-                Majored in Computer Science with a concentration in Game Development. 3.2 GPA. I wasn&apos;t the best student but most of the time, I put my whole ass into getting the degree. In the last two years of my degree, I balanced my internship with full-time studies to ensure that my skills were industry ready by graduation.
+                Majored in Computer Science with a concentration in Game Development. I balanced my internship with full-time studies to ensure that I was industry ready by graduation.
               </p>
             </ExperienceItem>
           </LabelledSection>
