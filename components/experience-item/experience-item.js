1a60434015b352760d2bf8401877cd9a6b64fec8
Albert Chu <albertchu539@gmail.com>
2022-04-18 16:43:36 -0600

    Collapsable experience items and responsive fixes

--- components/experience-item/experience-item.js
+++ components/experience-item/experience-item.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ArrowUpRightFromSquareIcon } from '../arrow-up-right-from-square-icon'
 import styles from './experience-item.module.css'
+import clsx from 'clsx'
+import { ChevronDownIcon } from '../chevron-down-icon';
 
 export const ExperienceItem = ({ logoPath, company, companyUrl, role, date, children }) => {
+    const [isExpanded, setIsExpanded] = useState(false)
+
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
+            <div onClick={() => setIsExpanded(!isExpanded)} className={styles.header}>
                 {logoPath && <img src={logoPath} />
                 }
                 <div className={styles.headerContents}>
@@ -21,9 +25,10 @@ export const ExperienceItem = ({ logoPath, company, companyUrl, role, date, chil
                         <div className={styles.role}>{role}</div>
                     </div>
                     <div className={styles.date}>{date}</div>
+                    <div className={clsx(styles.chevron, isExpanded && styles.chevronExpanded)}><ChevronDownIcon /></div>
                 </div>
             </div>
-            <div className={styles.details}>
+            <div className={clsx(styles.details, isExpanded && styles.detailsExpanded)}>
                 {children}
             </div>
         </div>
