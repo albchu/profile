4b263fa468d26114c3df90f90844d6792f741023
Albert Chu <albertchu539@gmail.com>
2022-04-18 22:39:12 -0600

    Expertise section roughly formatted

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,35 @@ export default function Home() {
 
             <p>If you are a prospective employer with an exciting opportunity, please include the phrase &quot;Pineapples belong on pizza&quot; somewhere in your email. This will show that you have taken the time to evaluate me for skill fit and I promise that I will evaluate your role as thoroughly as you have evaluated my profile.</p>
           </LabelledSection>
+
+          <LabelledSection title="Expertise">
+            <h5>React.js</h5>
+            <p>I have been developing with React.js since 2017. At this point in my career, I am very confident that I can be thrown into any random part of a React frontend codebase and be able to figure it out quickly.</p>
+
+            <br />
+
+            <h5>Node.js</h5>
+            <p>As much time as I have spent developing with React, I've been developing with Javascript for longer. From my University days of writing scrapers to liberate text books to building and deploying production Node.js applications across an entire stack, Javascript (and more specifically Node.js applications) have been the bread and butter of my career.</p>
+
+            <br />
+
+            <h5>Attention to detail</h5>
+
+            <p>An aspect of my professional career that I adore is collaborating with designers. It is important to me to ensure that their intent for a feature is realized fully. Over the course of my career, I have developed a keen eye for identifying all the little details of a design so that nothing gets lost in translation from Figma to prod deployment.</p>
+
+            <br />
+
+            <h5>Quality Software</h5>
+            <p>
+              Writing production quality software doesn't stop at keeping up with latest in the software development zeitgeist. You need to have high quality tests to ensure that functionality always works as intended.
+            </p>
+
+            <p>
+              As a developer that started in QA automation, I have always prioritized the importance of testing in development. As such, I have a lot of experience developing clear and testable code.
+            </p>
+          </LabelledSection>
+
+
           <LabelledSection title="Portfolio">
             <h5>Profile Website</h5>
             <p>I built this site as an excuse to try utilizing Next.js for server side generation. I designed and developed all aspects of this site. The source code can be located <a href="https://github.com/albchu/profile">here.</a></p>
@@ -65,7 +94,7 @@ export default function Home() {
             <br />
 
             <h5>Wedding Website</h5>
-            <p>In the before covid times, I was engaged to my fiancee (now Wife). As a fun project, we collaborated on building out our invites website. We wanted to make a mobile first application that reported RSVPs to a google sheet. I developed most of the technical aspects of the project while my wife designed the pages. The source code can be found <a href="https://github.com/albchu/wedding">here</a> and the most recent deployment of it can be found <a href="https://wedding-m80lv3z3t-albertchu1.vercel.app/">at this link</a>. Please excuse the random URL for the site. Domains are expensive and I have since let this one expire.</p>
+            <p>In the before covid times, I was engaged to my fiancee (now wife). As a fun project, we collaborated on building out our invites website. We wanted to make a mobile first application that reported RSVPs to a google sheet. I developed most of the technical aspects of the project while my wife designed the pages. The source code can be found <a href="https://github.com/albchu/wedding">here</a> and the most recent deployment of it can be found <a href="https://wedding-m80lv3z3t-albertchu1.vercel.app/">at this link</a>. Please excuse the random URL for the site. Domains are expensive and I have since let this one expire.</p>
 
             <br />
 
